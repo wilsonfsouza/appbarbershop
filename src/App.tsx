e48f9793915d847603d3fcd7e3bf9bf38ea1c41e
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-20 20:13:50 -0800

    chore: app config.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AppProvider from './hooks/index';
+
+import Routes from './routes';
+
+const App: React.FunctionComponent = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312E38" translucent />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312E38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
+  </NavigationContainer>
+);
+
+export default App;
